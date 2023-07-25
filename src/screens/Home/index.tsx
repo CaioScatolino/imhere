f905fcc678dabd6e0b2dfaf226d5e425f1bc2ecf
Caio <caio.scatolino@outlook.com>
2023-07-25 16:33:54 -0300

    Adicionando input

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,24 @@
-import { Text, View } from 'react-native'
+import { Text, View, TextInput } from 'react-native'
 import { styles } from './styles'
 
-export default function Home() {
+export function Home() {
 
     return (
-      <View style={styles.container}>
-        <Text key="1" style={styles.eventName}>
-          Nome do Evento
-        </Text>
-        <Text key="2" style={styles.eventDate}>
-          Sexta, 4 de Novembro de 2022.
-        </Text>
-      </View>
+        <View style={styles.container}>
+            <Text key="1" style={styles.eventName}>
+                Nome do Evento
+            </Text>
+            <Text key="2" style={styles.eventDate}>
+                Sexta, 4 de Novembro de 2022.
+            </Text>
+
+            <TextInput
+                style={styles.input}
+                placeholder="Nome do Participante"
+                placeholderTextColor="#6B6B6B"
+                keyboardType='default'
+            />
+        </View>
     )
-  
-  }
+
+}
