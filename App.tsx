0937e935164b850a502889c7e9fc79fd60f6ea5f
Caio <caio.scatolino@outlook.com>
2023-07-25 15:46:15 -0300

    Atualizando aulas

--- App.tsx
+++ App.tsx
@@ -1,12 +1,38 @@
-import { Text, View } from 'react-native'
+import { Text, View, StyleSheet } from 'react-native'
 
 export default function App() {
 
   return (
-    <View>
-      <Text key="1">React Native</Text>
-      <Text key="2">React Native</Text>
+    <View style={styles.container}>
+      <Text key="1" style={styles.eventName}>
+        Nome do Evento
+      </Text>
+      <Text key="2" style={styles.eventDate}>
+        Sexta, 4 de Novembro de 2022.
+      </Text>
     </View>
   )
 
 }
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: '#131016',
+    padding: 24,
+  },
+
+  eventName: {
+    color: '#FFF',
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 48,
+  },
+
+  eventDate: {
+    color: '#6B6B6B',
+    fontSize: 16,
+  }
+
+});
